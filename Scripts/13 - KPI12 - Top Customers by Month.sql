b3bd4c5b85b524db600d74e1256e396b3a8bb487
Kevin Feasel <feaselkl@gmail.com>
2023-05-03 20:42:22 -0400

    Finish the story

--- Scripts/13 - KPI12 - Top Customers by Month.sql
+++ Scripts/13 - KPI12 - Top Customers by Month.sql
@@ -67,6 +67,8 @@ SELECT TOP(50)
 	c.DayName,
 	ROW_NUMBER() OVER (PARTITION BY c.DayName ORDER BY c.Date ASC) AS rownum
 FROM dbo.Calendar c
+WHERE
+	c.FirstDayOfMonth = '1901-01-01'
 ORDER BY
 	c.DayName,
 	c.Date;
@@ -125,5 +127,44 @@ SELECT
 FROM CustomerResults cr
 	INNER JOIN Sales.Customers sc
 		ON cr.CustomerID = sc.CustomerID
+ORDER BY
+	RowNum ASC;
+
+WITH CustomerResults AS
+(
+	SELECT
+		o.CustomerID,
+		COUNT(*) AS NumberOfOrders
+	FROM Sales.Orders o
+		INNER JOIN dbo.Calendar c
+			ON o.OrderDate = c.Date
+	WHERE
+		c.FirstDayOfMonth = '2015-08-01'
+	GROUP BY
+		o.CustomerID
+),
+rankings AS
+(
+	SELECT
+		cr.CustomerID,
+		sc.CustomerName,
+		cr.NumberOfOrders,
+		ROW_NUMBER() OVER (ORDER BY cr.NumberOfOrders DESC) AS RowNum,
+		RANK() OVER (ORDER BY cr.NumberOfOrders DESC) AS Ranking,
+		DENSE_RANK() OVER (ORDER BY cr.NumberOfOrders DESC) AS DenseRanking
+	FROM CustomerResults cr
+		INNER JOIN Sales.Customers sc
+			ON cr.CustomerID = sc.CustomerID
+)
+SELECT
+	[r].[CustomerID],
+	[r].[CustomerName],
+	[r].[NumberOfOrders],
+	[r].[RowNum],
+	[r].[Ranking],
+	[r].[DenseRanking]
+FROM rankings r
+WHERE
+	r.Ranking <= 10
 ORDER BY
 	RowNum ASC;
